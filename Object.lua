5bea662275dd2eaeaeb89278ef8e4e8aaea30b5e
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-14 12:45:51 +0200

    Support for embedding

--- Object.lua
+++ Object.lua
@@ -1,116 +1,116 @@
-local function copy(object)
-  local result = {}
-  for key, value in pairs(object) do
-    result[key] = value
-  end
-  return result
-end
+local addOnName = 'Object'
+local version = '1.0.0'
 
-local function keys(object)
-  local result = {}
-  for key, _ in pairs(object) do
-    table.insert(result, key)
-  end
-  return result
-end
+if (_G.Library and not Library.isRegistered(addOnName, version)) or not _G.Library then
+  --- @class Object
+  local Object = {}
 
-local function values(object)
-  local result = {}
-  for _, value in pairs(object) do
-    table.insert(result, value)
+  function Object.copy(object)
+    local result = {}
+    for key, value in pairs(object) do
+      result[key] = value
+    end
+    return result
   end
-  return result
-end
 
-local function entries(object)
-  local list = {}
-  for key, value in pairs(object) do
-    table.insert(list, { key = key, value = value })
+  function Object.keys(object)
+    local result = {}
+    for key, _ in pairs(object) do
+      table.insert(result, key)
+    end
+    return result
   end
-  return list
-end
 
-local function equals(a, b)
-  local keysA = Set.create(keys(a))
-  local keysB = Set.create(keys(b))
-  if not Set.equals(keysA, keysB) then
-    return false
+  function Object.values(object)
+    local result = {}
+    for _, value in pairs(object) do
+      table.insert(result, value)
+    end
+    return result
   end
-  local keys = keysA
-  for key in Set.iterator(keys) do
-    if a[key] ~= b[key] then
+
+  function Object.entries(object)
+    local list = {}
+    for key, value in pairs(object) do
+      table.insert(list, { key = key, value = value })
+    end
+    return list
+  end
+
+  function Object.equals(a, b)
+    local keysA = Set.create(keys(a))
+    local keysB = Set.create(keys(b))
+    if not Set.equals(keysA, keysB) then
       return false
     end
+    local keys = keysA
+    for key in Set.iterator(keys) do
+      if a[key] ~= b[key] then
+        return false
+      end
+    end
+    return true
   end
-  return true
-end
 
-local function assignObject(object, objectB)
-  for key, value in pairs(objectB) do
-    object[key] = value
-  end
-end
-
-local function assign(object, ...)
-  local objects = { ... }
-  for _, objectB in ipairs(objects) do
-    assignObject(object, objectB)
-  end
-  return object
-end
-
-local function count(object)
-  local count = 0
-  for _, _ in pairs(object) do
-    count = count + 1
-  end
-  return count
-end
-
-local function every(object, prefix)
-  for key, value in pairs(object) do
-    if not prefix(value, key) then
-      return false
+  local function assignObject(object, objectB)
+    for key, value in pairs(objectB) do
+      object[key] = value
     end
   end
 
-  return true
-end
-
-local function fromEntries(entries)
-  local object = {}
-  for _, entry in ipairs(entries) do
-    object[entry.key] = entry.value
+  function Object.assign(object, ...)
+    local objects = { ... }
+    for _, objectB in ipairs(objects) do
+      assignObject(object, objectB)
+    end
+    return object
   end
-  return object
+
+  function Object.count(object)
+    local count = 0
+    for _, _ in pairs(object) do
+      count = count + 1
+    end
+    return count
+  end
+
+  function Object.every(object, prefix)
+    for key, value in pairs(object) do
+      if not prefix(value, key) then
+        return false
+      end
+    end
+
+    return true
+  end
+
+  function Object.fromEntries(entries)
+    local object = {}
+    for _, entry in ipairs(entries) do
+      object[entry.key] = entry.value
+    end
+    return object
+  end
+
+  function Object.isEmpty(object)
+    return not next(object)
+  end
+
+  function Object.remove(object, key)
+    object[key] = nil
+  end
+
+  function Object.hasKeys(object)
+    return Boolean.toBoolean(next(object))
+  end
+
+  Object.hasEntries = Object.hasKeys
+
+  if _G.Library then
+    Library.register(addOnName, version, Object)
+  else
+    _G.Object = Object
+    --- @deprecated
+    _G.Object2 = Object
+  end
 end
-
-local function isEmpty(object)
-  return not next(object)
-end
-
-local function remove(object, key)
-  object[key] = nil
-end
-
-local function hasKeys(object)
-  return Boolean.toBoolean(next(object))
-end
-
-Object = {
-  copy = copy,
-  keys = keys,
-  values = values,
-  entries = entries,
-  equals = equals,
-  assign = assign,
-  count = count,
-  every = every,
-  fromEntries = fromEntries,
-  isEmpty = isEmpty,
-  remove = remove,
-  hasKeys = hasKeys,
-  hasEntries = hasKeys,
-}
-
-Object2 = Object
--- README.md
+++ README.md
@@ -21,4 +21,46 @@ An object is regarded to be a Lua table with zero or more key-value pairs.
 * **Object.remove**: a function for removing a key-value pair with a specific key from an object.
 * **Object.hasKeys** / **Object.hasEntries**: a function to check if an object has any keys.
 
+## Usage
+
+### Embedding
+
+#### Introduction
+
+With this approach, the library is included as part of the add-on. A benefit to the add-on user is, that only
+the add-on appears in the add-on list. So it can be easier to enable or disable an add-on and its library dependencies
+compared to an approach where the libraries are included as add-ons (in this case each library shows as an add-on in the add-ons list and the user potentially is required to tick multiple checkboxes to enable or disable an add-on and its library dependencies).
+
+#### How to do it
+
+This library registers itself via the library "[Library](https://github.com/SanjoSolutions/LuaLibrary)". With this approach it is avoided to create another global (with its theoretical potential that other add-ons already use the same global). And "Library" also handles when multiple add-ons load the same library, potentially different versions of it.
+To embed it, also include "[Library](https://github.com/SanjoSolutions/LuaLibrary)" as a dependency to your add-on.
+
+In our example, we have installed both "Library", "Object" and the dependencies of "Object" ("Boolean" and "Set") inside the "libs" folder inside the add-on folder.
+
+In your add-ons TOC file add (to load "Library" and "Object" and its dependencies):
+
+```
+libs/Library/Library.xml
+libs/Boolean/Boolean.xml
+libs/Set/Set.xml
+libs/Object/Object.xml
+```
+
+It's recommended to load the library files first, so that the libraries are available in the other Lua files.
+
+In your Lua file(s), you can access the library via:
+
+```lua
+local Object = Library.retrieve('Object', '^1.0.0')
+```
+
+### As add-on
+
+This library can also just be put directly in the AddOns folder. In this case the library is available via the global `Object`.
+
+If you develop an add-on and do this approach, you can just include this library in your release as a folder next to your add-ons folder.
+
+## Support
+
 You can support me on [Patreon](https://www.patreon.com/addons_by_sanjo).
